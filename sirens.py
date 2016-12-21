bfe55c5f45b5d1ed85511688513badbcb44e1a56
Ian Thompson <ianthompson@nicelion.com>
2016-12-21 15:32:27 -0500

    added sirens.py

---
+++ sirens.py
@@ -0,0 +1,444 @@
+# sirens.py
+#
+# by Ian Thompson on 12/2/16
+# Part of the Piren Project
+#
+# (c) 2016 Ian Thompson
+
+
+# Code 3
+class Code_3:
+
+    class _3932_Scorpion:
+        def horn():
+            return 'sirens/Code 3/3932 Scorpion/3932horn.wav'
+        def wail():
+            return 'sirens/Code 3/3932 Scorpion/3932wail.wav'
+        def yelp():
+            return 'sirens/Code 3/3932 Scorpion/3932yelp.wav'
+        def phaser():
+            return 'sirens/Code 3/3932 Scorpion/3932Hyelp.wav'
+
+    class Mastercom:
+        def horn():
+            return 'sirens/Code 3/Mastercom/mchorn.wav'
+        def wail():
+            return 'sirens/Code 3/Mastercom/mchylo.wav'
+        def yelp():
+            return 'sirens/Code 3/Mastercom/mchilo.wav'
+        def phaser():
+            return 'sirens/Code 3/Mastercom/mcHyelp.wav'
+    
+    class Mastercom_B:
+        def horn():
+            return 'sirens/Code 3/Mastercom B/mcbhorn.wav'
+        def wail(): 
+            return 'sirens/Code 3/Mastercom B/mcbwail.wav'        
+        def yelp(): 
+            return 'sirens/Code 3/Mastercom B/mcbyelp.wav'
+        def phaser(): 
+            return 'sirens/Code 3/Mastercom B/mcbHyelp.wav'
+        def hylo(): 
+            return 'sirens/Code 3/Mastercom B/mcbhylo.wav'
+        def hilo():
+            return 'sirens/Code 3/Mastercom B/mcbhilo.wav'
+
+    class RLS:
+        def horn():
+            return 'sirens/Code 3/RLS/rlshorn.wav'
+        def wail(): 
+            return 'sirens/Code 3/RLS/rlswail.wav'        
+        def yelp(): 
+            return 'sirens/Code 3/RLS/rlsyelp.wav'
+        def phaser(): 
+            return 'sirens/Code 3/RLS/rlsHyelp.wav'
+        def hilo():
+            return 'sirens/Code 3/RLS/rlshilo.wav'
+    
+    class Vcon:
+        def horn():
+            return 'sirens/Code 3/Vcon/vconhorn.wav'
+        def wail(): 
+            return 'sirens/Code 3/Vcon/vconwail.wav'        
+        def yelp(): 
+            return 'sirens/Code 3/Vcon/vconyelp.wav'
+        def phaser(): 
+            return 'sirens/Code 3/Vcon/vconHyelp.wav'
+        def hilo():
+            return 'sirens/Code 3/Vcon/vconhilo.wav'
+
+# Federal Signal Sirens
+class Federal_Signal:
+
+    class EQ2B:
+        def horn():
+            return 'sirens/Federal Signal/EQ2B/eq2bhorn.wav'
+        def wail():
+            return 'sirens/Federal Signal/EQ2B/eq2bwail.wav'
+        def yelp():
+            return 'sirens/Federal Signal/EQ2B/eq2byelp.wav'
+        def phaser():
+            return 'sirens/Federal Signal/EQ2B/eq2bprty.wav'
+    
+    class MS4000:
+        def horn():
+            return 'sirens/Federal Signal/MS4000/ms4000horn.wav'
+        def wail():
+            return 'sirens/Federal Signal/MS4000/ms4000wail.wav'
+        def yelp():
+            return 'sirens/Federal Signal/MS4000/ms4000yelp.wav'
+        def phaser():
+            return 'sirens/Federal Signal/MS4000/ms4000prty.wav'
+
+    class PA20:
+        def horn():
+            return 'sirens/Federal Signal/PA20/pa20horn.wav'
+        def wail():
+            return 'sirens/Federal Signal/PA20/pa20horn.wav'
+        def yelp():
+            return 'sirens/Federal Signal/PA20/pa20yelp.wav'
+        def yelp2():
+            return 'sirens/Federal Signal/PA20/pa20yelp2.wav'
+        def yelp3():
+            return 'sirens/Federal Signal/PA20/pa20yelp3.wav'
+        def hilo():
+            return 'sirens/Federal Signal/PA20/pa20hilo.wav'
+
+    class PA150:
+        def hilo():
+            return 'sirens/Federal Signal/PA150/pa150hilo.wav'
+        def wail():
+            return 'sirens/Federal Signal/PA150/pa150wail.wav'
+        def yelp():
+            return 'sirens/Federal Signal/PA150/pa150yelp.wav'
+
+    class PA300:
+        def hilo():
+            return 'sirens/Federal Signal/PA300/300hilo.wav'
+        def horn():
+            return 'sirens/Federal Signal/PA300/300horn.wav'
+        def phaser():
+            return 'sirens/Federal Signal/PA300/300prty.wav'
+        def wail():
+            return 'sirens/Federal Signal/PA300/300wail.wav'
+        def yelp():
+            return 'sirens/Federal Signal/PA300/300yelp.wav'
+
+    class PA640:
+        def horn():
+            return 'sirens/Federal Signal/PA640/pa640horn.wav'
+        def phaser():
+            return 'sirens/Federal Signal/PA640/pa640prty.wav'
+        def wail():
+            return 'sirens/Federal Signal/PA640/pa640wail.wav'
+        def yelp():
+            return 'sirens/Federal Signal/PA640/pa640yelp.wav'
+    
+    class PA4000:
+        def horn():
+            return 'sirens/Federal Signal/PA4000/pa4000horn.wav'
+        def phaser():
+            return 'sirens/Federal Signal/PA4000/pa4000prty.wav'
+        def wail():
+            return 'sirens/Federal Signal/PA4000/pa4000wail.wav'
+        def yelp():
+            return 'sirens/Federal Signal/PA4000/pa4000yelp.wav'
+
+    class SS200_Mini:
+        def horn():
+            return 'sirens/Federal Signal/SS200 Mini/ss200horn.wav'
+        def wail():
+            return 'sirens/Federal Signal/SS200 Mini/ss200wail.wav'
+        def yelp():
+            return 'sirens/Federal Signal/SS200 Mini/ss200yelp.wav'
+
+    class SS2000SM:
+        def hilo():
+            return 'sirens/Federal Signal/SS2000SM Smart Siren/sshilo.wav'
+        def horn():
+            return 'sirens/Federal Signal/SS2000SM Smart Siren/sshorn.wav'
+        def phaser():
+            return 'sirens/Federal Signal/SS2000SM Smart Siren/ssprty.wav'
+        def wail():
+            return 'sirens/Federal Signal/SS2000SM Smart Siren/sswail.wav'
+        def yelp():
+            return 'sirens/Federal Signal/SS2000SM Smart Siren/ssyelp.wav'
+
+    class SSP3000b:
+        def hilo():
+            return 'sirens/Federal Signal/SSP3000b Smart Siren Platinum/smarthilo.wav'
+        def horn():
+            return 'sirens/Federal Signal/SSP3000b Smart Siren Platinum/smarthorn.wav'
+        def phaser():
+            return 'sirens/Federal Signal/SSP3000b Smart Siren Platinum/smartprty.wav'
+        def wail():
+            return 'sirens/Federal Signal/SSP3000b Smart Siren Platinum/smartwail.wav'
+        def yelp():
+            return 'sirens/Federal Signal/SSP3000b Smart Siren Platinum/smartyelp.wav'
+   
+    class SSP3000b_w_Rumbler:
+        def horn():
+            return 'sirens/Federal Signal/SSP3000b Smart Siren Platinum/smarthorn.wav'
+        def phaser():
+            return 'sirens/Federal Signal/SSP3000b with Rumbler/rumblerprty.wav'
+        def wail():
+            return 'sirens/Federal Signal/SSP3000b with Rumbler/rumblerwail.wav'
+        def yelp():
+            return 'sirens/Federal Signal/SSP3000b with Rumbler/rumbleryelp.wav'
+
+    class Touchmaster_Touchmaster_Delta:
+        def hetro():
+            return 'sirens/Federal Signal/Touchmaster_Touchmaster Delta/tmhetro.wav'
+        def hilo():
+            return 'sirens/Federal Signal/Touchmaster_Touchmaster Delta/tmhilo.wav'
+        def horn():
+            return 'sirens/Federal Signal/Touchmaster_Touchmaster Delta/tmhorn.wav'
+        def phaser():
+            return 'sirens/Federal Signal/Touchmaster_Touchmaster Delta/tmprty.wav'
+        def sweep1():
+            return 'sirens/Federal Signal/Touchmaster_Touchmaster Delta/tmsweep1.wav'
+        def sweep2():
+            return 'sirens/Federal Signal/Touchmaster_Touchmaster Delta/tmsweep2.wav'
+        def Uhilo():
+            return 'sirens/Federal Signal/Touchmaster_Touchmaster Delta/tmUhilo.wav'
+        def wail():
+            return 'sirens/Federal Signal/Touchmaster_Touchmaster Delta/tmwail.wav'
+        def yelp():
+            return 'sirens/Federal Signal/Touchmaster_Touchmaster Delta/tmyelp.wav'
+
+    class Unitrol_80k:
+        def hetro():
+            return 'sirens/Federal Signal/Unitrol 80k/80hetro.wav'
+        def hilo():
+            return 'sirens/Federal Signal/Unitrol 80k/80hilo.wav'
+        def horn():
+            return 'sirens/Federal Signal/Unitrol 80k/80horn.wav'
+        def wail():
+            return 'sirens/Federal Signal/Unitrol 80k/80wail.wav'
+        def yelp():
+            return 'sirens/Federal Signal/Unitrol 80k/80yelp.wav'
+
+    class Unitrol_8001:
+        def hilo():
+            return 'sirens/Federal Signal/Unitrol 8001/8001hilo.wav'
+        def horn():
+            return 'sirens/Federal Signal/Unitrol 8001/8001horn.wav'
+        def phaser():
+            return 'sirens/Federal Signal/Unitrol 8001/8001prty.wav'
+        def sweep():
+            return 'sirens/Federal Signal/Unitrol 8001/8001sweep.wav'
+        def wail():
+            return 'sirens/Federal Signal/Unitrol 8001/8001wail.wav'
+        def yelp():
+            return 'sirens/Federal Signal/Unitrol 8001/8001yelp.wav'
+
+    class Unitrol_Omega_90:
+        def hetro():
+            return 'sirens/Federal Signal/Unitrol Omega 90/o90hetro.wav'
+        def hilo():
+            return 'sirens/Federal Signal/Unitrol Omega 90/o90hilo.wav'
+        def horn():
+            return 'sirens/Federal Signal/Unitrol Omega 90/o90horn.wav'
+        def scan():
+            return 'sirens/Federal Signal/Unitrol Omega 90/o90scan.wav'
+        def wail():
+            return 'sirens/Federal Signal/Unitrol Omega 90/o90wail.wav'
+        def yelp():
+            return 'sirens/Federal Signal/Unitrol Omega 90/o90yelp.wav'
+
+# Galls
+class Galls:
+
+    class ST160_Street_Thunder:
+        def hilo():
+            return 'sirens/Galls/ST160 Street Thunder/st160hilo.wav'
+        def horn():
+            return 'sirens/Galls/ST160 Street Thunder/st160horn.wav'
+        def thunder():
+            return 'sirens/Galls/ST160 Street Thunder/st160thunder.wav'
+        def wail():
+            return 'sirens/Galls/ST160 Street Thunder/st160wail.wav'
+        def yelp():
+            return 'sirens/Galls/ST160 Street Thunder/st160yelp.wav'
+
+    class ST300_Command_Center:
+        def hilo():
+            return 'sirens/Galls/ST300 Command Center/st300hilo.wav'
+        def horn():
+            return 'sirens/Galls/ST300 Command Center/st300horn.wav'
+        def phaser():
+            return 'sirens/Galls/ST300 Command Center/st300phsr.wav'
+        def wail():
+            return 'sirens/Galls/ST300 Command Center/st300wail.wav'
+        def yelp():
+            return 'sirens/Galls/ST300 Command Center/st300yelp.wav'
+
+
+
+# Whelen
+class Whelen:
+
+    class _295HF100:
+        def wail():
+            return 'sirens/Whelen/295HF100/hf100wail.wav'
+        def horn():
+            return 'sirens/Whelen/295HF100/hf100horn.wav'
+        def yelp():
+            return 'sirens/Whelen/295HF100/hf100yelp.wav'
+        def phaser():
+            return 'sirens/Whelen/295HF100/hf100pier.wav'
+
+    class _295SLSA1:
+        pass
+
+    class Alpha:
+        def hilo():
+            return 'sirens/Whelen/Alpha/alphahilo.wav'
+        def horn():
+            return 'sirens/Whelen/Alpha/alphahorn.wav'
+        def phaser():
+            return 'sirens/Whelen/Alpha/alphapier.wav'
+        def wail():
+            return 'sirens/Whelen/Alpha/alphawail.wav'
+        def yelp():
+            return 'sirens/Whelen/Alpha/alphayelp.wav'
+
+    class Alpha_22m:
+        def horn():
+            return 'sirens/Whelen/Alpha 22m Mechanical/22mhorn.wav'
+        def power_call():
+            return 'sirens/Whelen/Alpha 22m Mechanical/22mpcall.wav'
+        def power_call_2():
+            return 'sirens/Whelen/Alpha 22m Mechanical/22mpcall2.wav'
+        def wail():
+            return 'sirens/Whelen/Alpha 22m Mechanical/22mwail.wav'
+        def wail2():
+            return 'sirens/Whelen/Alpha 22m Mechanical/22mwailm.wav'
+
+    class Beta:
+        def horn():
+            return 'sirens/Whelen/Beta/betahorn.wav'
+        def phaser():
+            return 'sirens/Whelen/Beta/betapier.wav'
+        def wail():
+            return 'sirens/Whelen/Beta/betawail.wav'
+        def yelp():
+            return 'sirens/Whelen/Beta/betayelp.wav'
+
+    class Cencom_Gold:
+        def hilo():
+            return 'sirens/Whelen/Cencom Gold/ccghilo.wav'
+        def horn():
+            return 'sirens/Whelen/Cencom Gold/ccghorn.wav'
+        def phaser():
+            return 'sirens/Whelen/Cencom Gold/ccgpier.wav'
+        def wail():
+            return 'sirens/Whelen/Cencom Gold/ccgwail.wav'
+        def yelp():
+            return 'sirens/Whelen/Cencom Gold/ccgyelp.wav'
+
+    class Cencom_Sapphire:
+        def horn():
+            return 'sirens/Whelen/Cencom Sapphire/ccshorn.wav'
+        def phaser():
+            return 'sirens/Whelen/Cencom Sapphire/ccspier.wav'
+        def wail():
+            return 'sirens/Whelen/Cencom Sapphire/ccswail.wav'
+        def yelp():
+            return 'sirens/Whelen/Cencom Sapphire/ccsyelp.wav'
+    
+    class Cencom_Sapphire_Howler:
+        def horn():
+            return 'sirens/Whelen/Cencom Sapphire Howler/howlerhorn.wav'
+        def phaser():
+            return 'sirens/Whelen/Cencom Sapphire Howler/howlerpier.wav'
+        def wail():
+            return 'sirens/Whelen/Cencom Sapphire Howler/howlerwail.wav'
+        def yelp():
+            return 'sirens/Whelen/Cencom Sapphire Howler/howleryelp.wav'
+
+    class Epsilon_EPSL_1:
+        def hilo():
+            return 'sirens/Whelen/Epsilon EPSL-1/epslhilo.wav'
+        def horn():
+            return 'sirens/Whelen/Epsilon EPSL-1/epslhorn.wav'
+        def phaser():
+            return 'sirens/Whelen/Epsilon EPSL-1/epslpier.wav'
+        def wail():
+            return 'sirens/Whelen/Epsilon EPSL-1/epslwail.wav'
+        def yelp():
+            return 'sirens/Whelen/Epsilon EPSL-1/epslyelp.wav'
+
+    class Gamma_2:
+        def horn():
+            return 'sirens/Whelen/Gamma 2/gam2horn.wav'
+        def phaser():
+            return 'sirens/Whelen/Gamma 2/gam2pier.wav'
+        def riot():
+            return 'sirens/Whelen/Gamma 2/gam2riot.wav'
+        def wail():
+            return 'sirens/Whelen/Gamma 2/gam2wail.wav'
+        def warble():
+            return 'sirens/Whelen/Gamma 2/gam2warble.wav'
+
+class Other:
+    class Air_Horns:
+        def horn():
+            return 'sirens/Other/Air Horns/genericgrover.wav'
+        def train():
+            return 'sirens/Other/Air Horns/k5la.wav'
+    
+    class AS350_Heli:
+        def siren():
+            return 'sirens/Other/AS350 Helicopter Siren/helisiren.wav'
+
+    class Carson_SA411:
+        def hilo():
+            return 'sirens/Other/Carson SA411/sa411hilo.wav'
+        def horn():
+            return 'sirens/Other/Carson SA411/sa411horn.wav'
+        def phaser():
+            return 'sirens/Other/Carson SA411/sa411phaser.wav'
+        def wail():
+            return 'sirens/Other/Carson SA411/sa411wail.wav'
+        def yelp():
+            return 'sirens/Other/Carson SA411/sa411yelp.wav'
+
+    class NA_SI100M:
+        def hilo():
+            return 'sirens/Other/North American SI100M/nahilo.wav'
+        def riot():
+            return 'sirens/Other/North American SI100M/nahilo.wav'
+        def wail():
+            return 'sirens/Other/North American SI100M/nawail.wav'
+        def yelp():
+            return 'sirens/Other/North American SI100M/nayelp.wav'
+
+    class Powercall_DX5:
+        def hilo():
+            return 'sirens/Other/Powercall DX5/dx5hilo.wav'
+        def horn():
+            return 'sirens/Other/Powercall DX5/dx5horn.wav'
+        def inter():
+            return 'sirens/Other/Powercall DX5/dx5inter.wav'
+        def pcall():
+            return 'sirens/Other/Powercall DX5/dx5pcall.wav'
+        def pcall2():
+            return 'sirens/Other/Powercall DX5/dx5pcallp.wav'
+        def phaser():
+            return 'sirens/Other/Powercall DX5/dx5phsr.wav'
+        def wail():
+            return 'sirens/Other/Powercall DX5/dx5wail.wav'
+        def yelp():
+            return 'sirens/Other/Powercall DX5/dx5yelp.wav'
+
+    class Unitrol_480k:
+        def hilo():
+            return 'sirens/Other/Unitrol 480k/480hilo.wav'
+        def horn():
+            return 'sirens/Other/Unitrol 480k/480horn.wav'
+        def wail():
+            return 'sirens/Other/Unitrol 480k/480wail.wav'
+        def yelp():
+            return 'sirens/Other/Unitrol 480k/480yelp.wav'
+
