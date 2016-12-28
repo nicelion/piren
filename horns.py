88809237646c76a80f247a7152d9f60458be51de
Ian Thompson <ianthompson@nicelion.com>
2016-12-28 00:07:20 -0500

    added mapping for all horns

---
+++ horns.py
@@ -0,0 +1,43 @@
+# horns.py
+#
+# by Ian Thompson on 12/2/16
+# Part of the Piren Project
+#
+# (c) 2016 Ian Thompson
+
+
+def dixie_horn():
+    return 'horns/dixie-horn.wav'
+
+
+def GTA4_Horn():
+    return 'horns/GTA4-Horn.wav'
+
+
+def ice_cream():
+    return 'horns/ice-cream-truck.wav'
+
+def bad_boys():
+    return 'horns/probadboys.wav'
+
+def ecto1():
+    return 'horns/proecto1.wav'
+
+def move():
+    return 'horns/promove.wav'
+
+def pull_over():
+    return 'horns/propull.wav'
+
+
+def rap_horn():
+    return 'horns/rap-horn.wav'
+
+def trombone():
+    return 'horns/sad trombone.wav'
+
+def train_horn():
+    return 'horns/k5la.wav'
+
+def horn():
+    return 'horns/genericgrover.wav'
